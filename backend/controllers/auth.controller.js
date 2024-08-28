b684058fb5e9e244eb7a6cad07dcac860cd5d18a
Anand P <anandpamegoze@gmail.com>
2024-08-28 16:16:59 +0530

    generate JWT token and set cookie when sign up

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import { User } from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
+import { generateTokenAndSetCookie } from '../utils/generateToken.js';
 
 export async function signup(req, res) {
   try {
@@ -48,7 +49,10 @@ export async function signup(req, res) {
       username: username,
       image: image,
     });
+
+    generateTokenAndSetCookie(newUser._id, res);
     await newUser.save();
+
     res
       .status(201)
       .json({ success: true, user: { ...newUser._doc, password: '' } }); //removing pswd from response
