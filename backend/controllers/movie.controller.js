b9733fb716f0ac419056767c0b2b30747e4ee305
Anand P <anandpamegoze@gmail.com>
2024-08-29 11:55:43 +0530

    getMovieDetails

--- backend/controllers/movie.controller.js
+++ backend/controllers/movie.controller.js
@@ -28,3 +28,18 @@ export async function getMovieTrailers(req, res) {
     res.status(500).json({ success: false, message: 'Internal server error' });
   }
 }
+
+export async function getMovieDetails(req, res) {
+  const { id } = req.params;
+  try {
+    const data = await fetchFromTMDB(
+      `https://api.themoviedb.org/3/movie/${id}?language=en-US`
+    );
+    res.status(200).json({ success: true, content: data });
+  } catch (error) {
+    if (error.message.includes('404')) {
+      return res.status(404).send(null);
+    }
+    res.status(500).json({ success: false, message: 'Internal server error' });
+  }
+}
