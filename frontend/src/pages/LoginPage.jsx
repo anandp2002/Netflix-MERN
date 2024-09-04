1601afb8b7cbe9f50239fd9f4484e5228612b71f
Anand P <anandpamegoze@gmail.com>
2024-09-04 23:08:33 +0530

    add Loading... while logging in and signup

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -5,7 +5,7 @@ import { useAuthStore } from '../store/authUser';
 const LoginPage = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const { login } = useAuthStore();
+  const { login, isLoggingIn } = useAuthStore();
 
   const handleLogin = (e) => {
     e.preventDefault();
@@ -47,7 +47,7 @@ const LoginPage = () => {
               />
             </div>
             <button className="w-full py-2 bg-red-600 text-white font-semibold rounded-md hover:bg-red-700">
-              Login
+              {isLoggingIn ? 'Loading...' : 'Login'}
             </button>
           </form>
           <div className="text-center text-gray-400">
